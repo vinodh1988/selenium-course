d3b8c69f1e749b976f50c756b7852d3fafcb1bbd
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-13 16:52:10 +0530

    google-activity

---
+++ src/com/sample/Activity.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Activity {
+    public static void main(String[] args) {
+		WebDriver wd = DriverProvider.getDriver();
+		wd.get("http://google.co.in");
+	    WebElement element =	wd.findElement(By.name("q"));
+	    element.sendKeys("India");
+	    element.sendKeys(Keys.RETURN);
+	   
+	}
+}
