3c1af6c8299046bd6fc25915c2f88faa2540dec4
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-14 16:43:42 +0530

    radio-commit

--- src/com/forms/CalorieCalculator.java
+++ src/com/forms/CalorieCalculator.java
@@ -1,5 +1,7 @@
 package com.forms;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,11 +13,15 @@ public class CalorieCalculator {
 	   WebDriver wd=DriverProvider.getDriver();
        wd.get("https://www.calculator.net/calorie-calculator.html");
        WebElement age =  wd.findElement(By.name("cage"));
-       WebElement gender1 =  wd.findElement(By.id("csex1"));
-       WebElement gender2 =  wd.findElement(By.id("csex2"));
+       //wd.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+      // WebElement gender1 =  wd.findElement(By.id("csex1"));
+       // WebElement gender2 =  wd.findElement(By.id("csex2"));
+       List<WebElement> radiobuttons=wd.findElements(By.className("rbmark"));
        age.clear();
        age.sendKeys("45");
-     
-       gender2.click();
+       System.out.println(radiobuttons.size());
+       radiobuttons.get(1).click();
+       //System.out.println(gender1.isDisplayed()+" "+gender2.isDisplayed());
+       //gender2.click();
   }
 }
