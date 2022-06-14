6cdc0136e84e4cb6bbcd19af18e9a0474e314766
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-14 17:12:02 +0530

    select

--- src/com/forms/CalorieCalculator.java
+++ src/com/forms/CalorieCalculator.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import com.sample.DriverProvider;
 
@@ -13,15 +14,26 @@ public class CalorieCalculator {
 	   WebDriver wd=DriverProvider.getDriver();
        wd.get("https://www.calculator.net/calorie-calculator.html");
        WebElement age =  wd.findElement(By.name("cage"));
-       //wd.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-      // WebElement gender1 =  wd.findElement(By.id("csex1"));
-       // WebElement gender2 =  wd.findElement(By.id("csex2"));
+       WebElement height = wd.findElement(By.name("cheightmeter"));
+       WebElement weight = wd.findElement(By.name("ckg"));
+       Select select = new Select( wd.findElement(By.name("cactivity")));
+       
        List<WebElement> radiobuttons=wd.findElements(By.className("rbmark"));
        age.clear();
        age.sendKeys("45");
        System.out.println(radiobuttons.size());
        radiobuttons.get(1).click();
+       height.clear();weight.clear();
+       height.sendKeys("185");
+       weight.sendKeys("69");
+       select.selectByIndex(4);
+       
+       WebElement button = wd.findElement(By.xpath("/html/body/div[3]/div[1]/div[5]/table[4]/tbody/tr[3]/td[2]/input[2]"));
+       button.click();
        //System.out.println(gender1.isDisplayed()+" "+gender2.isDisplayed());
        //gender2.click();
+     //wd.manage().timeouts().implicitlyWait(10,TimeUnit.SENDS);
+       // WebElement gender1 =  wd.findElement(By.id("csex1"));
+        // WebElement gender2 =  wd.findElement(By.id("csex2"));
   }
 }
