763703cdf42e5b7faf26ae599e995be8f1c3cf61
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-16 15:19:48 +0530

    select

---
+++ src/com/forms/Excecutor.java
@@ -0,0 +1,37 @@
+package com.forms;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+public class Excecutor {
+   public static void main(String[] args) throws Exception {
+	   Map<String,Object> preferences =new Hashtable<String,Object>();  
+	   preferences.put("download.prompt_for_download", false);
+	   preferences.put("download.default_directory", "d:\\books");
+	   
+	   ChromeOptions options = new ChromeOptions();
+	   options.setExperimentalOption("prefs", preferences);	
+	  
+	   System.setProperty("webdriver.chrome.driver", "E:/selenium/chromedriver.exe");
+	   WebDriver wd=new ChromeDriver(options);
+	   
+	   wd.get("https://ncert.nic.in/textbook.php");
+	   
+	  Select select1 =new Select(wd.findElement(By.name("tclass")));
+	  Select select2 =new Select(wd.findElement(By.name("tsubject")));
+	  Select select3 =new Select(wd.findElement(By.name("tbook")));
+	  
+	  select1.selectByIndex(10);
+	  select2.selectByIndex(5);
+	  select3.selectByIndex(1);
+	  //Thread.sleep(5000);
+	  wd.findElement(By.name("button")).click(); 
+	  
+   }
+}
