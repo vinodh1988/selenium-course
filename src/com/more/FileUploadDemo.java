df13f7539dcc03a17706c0d42e11670db40f1eb6
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-20 16:27:36 +0530

    file-upload-with-script

--- src/com/more/FileUploadDemo.java
+++ src/com/more/FileUploadDemo.java
@@ -1,15 +1,18 @@
 package com.more;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import com.sample.DriverProvider;
 
 public class FileUploadDemo {
-   public static void main(String n[]) {
+   public static void main(String n[]) throws IOException {
 	   WebDriver wd= DriverProvider.getDriver();
 	   wd.get("https://document.online-convert.com/convert-to-doc");
 	   wd.findElement(By.xpath("/html/body/div[4]/div[3]/div[1]/div/div/div/button"))
 	   .click();
+	   Runtime.getRuntime().exec("e:\\scripts\\script.exe");
    }
 }
