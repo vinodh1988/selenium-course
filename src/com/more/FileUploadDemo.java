d1d90818e68b422fde56d238472418f7cb0f197f
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-20 15:51:53 +0530

    file-upload

---
+++ src/com/more/FileUploadDemo.java
@@ -0,0 +1,15 @@
+package com.more;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.sample.DriverProvider;
+
+public class FileUploadDemo {
+   public static void main(String n[]) {
+	   WebDriver wd= DriverProvider.getDriver();
+	   wd.get("https://document.online-convert.com/convert-to-doc");
+	   wd.findElement(By.xpath("/html/body/div[4]/div[3]/div[1]/div/div/div/button"))
+	   .click();
+   }
+}
